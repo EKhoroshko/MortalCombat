7f270a9c3ad4ee802f08aa63630548cb1d367e87
EKhoroshko <xapoxa14@gmail.com>
2021-06-14 10:05:42 +0300

    time

--- main.js
+++ main.js
@@ -154,7 +154,14 @@ function showResult() {
 
 function timeToFight() {
     const date = new Date();
-    const time = date.getHours() + ':' + date.getMinutes();
+    function getZero(num) {
+        if (num > 0 && num < 10) {
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+    const time = getZero(date.getHours()) + ':' + getZero(date.getMinutes());
     return time;
 }
 

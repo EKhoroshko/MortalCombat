329b60a70b7999b043d562bcbe557688bfc085df
EKhoroshko <xapoxa14@gmail.com>
2021-06-30 20:51:11 +0300

    fix

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function init() {
       el.classList.add("active");
 
       setTimeout(() => {
-        window.location.pathname = "./arenas.html";
+        window.location.pathname = 'arenas.html';
       }, 1000);
     });
 
